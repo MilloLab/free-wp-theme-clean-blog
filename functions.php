a0e436f7f05820e452fd9e4a34a2c95f3fca08b2
Héctor Muñoz <hectormunozgarcia@gmail.com>
2015-04-29 23:13:26 +0100

    css enqueue fixed

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 function theme_styles() {
 
     wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-    wp_enqueue_style( 'style_css', get_template_directory_uri() . '/clean-blog.min.css' );
+    wp_enqueue_style( 'style_css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'font-awesome','http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
     wp_enqueue_style( 'google_font', 'http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' );
     wp_enqueue_style( 'google_font2','http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' );
