595c1b1a614d9438a9e1c73438bce7214833b5b3
Héctor Muñoz <hectormunozgarcia@gmail.com>
2015-05-01 19:53:12 +0100

    footer updated with dynamic social links info

--- footer.php
+++ footer.php
@@ -5,27 +5,52 @@
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <ul class="list-inline text-center">
+                    <?php if( get_theme_mod( 'custom_facebook_link') != "" ): ?>
                         <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
+                            <a href="<?php echo get_theme_mod( 'custom_facebook_link'); ?>" target="_blank">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
                                 </span>
                             </a>
                         </li>
+                    <?php endif; ?>
+
+                    <?php if( get_theme_mod( 'custom_twitter_link') != "" ): ?>     
                         <li>
-                            <a href="#">
+                            <a href="<?php echo get_theme_mod( 'custom_twitter_link'); ?>" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                    <?php if( get_theme_mod( 'custom_linkedin_link') != "" ): ?>    
+                        <li>
+                            <a href="<?php echo get_theme_mod( 'custom_linkedin_link'); ?>" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                    <?php if( get_theme_mod( 'custom_github_link') != "" ): ?>
+                        <li>
+                            <a href="<?php echo get_theme_mod( 'custom_github_link'); ?>" target="_blank">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    <?php endif; ?>         
+                        <li>
+                            <a href="/feed" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-rss fa-stack-1x fa-inverse"></i>
                                 </span>
                             </a>
                         </li>
